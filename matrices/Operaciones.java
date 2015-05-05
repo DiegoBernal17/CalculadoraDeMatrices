7f7a2938a7310112a00ea5f8e2b0c588d842d1a2
DiegoBernal17 <diego.bernal17@gmail.com>
2015-05-05 18:43:07 -0500

    Update Operaciones.java

--- matrices/Operaciones.java
+++ matrices/Operaciones.java
@@ -9,8 +9,8 @@ import fraccion.Fracciones;
  */
 public class Operaciones {
     
-    private int tamCols;
-    private int tamRows;
+    private final int tamCols;
+    private final int tamRows;
     private Fracciones[][] matriz;
     private Fracciones[][] matriz_2;
 
@@ -25,20 +25,20 @@ public class Operaciones {
     {
         this.tamCols = tamCols;
         this.tamRows = tamRows;
-        this.matriz = this.convertirFraccion(matriz);
-        
         this.matriz = new Fracciones[tamCols][tamRows];
+        this.matriz_2 = new Fracciones[tamCols][tamRows];
+        this.matriz = this.convertirFraccion(matriz);
     }
     
     public Operaciones(int tamCols, int tamRows, int[][] matriz, int tamRows_2, int tamCols_2, int[][] matriz_2)
     {
         this.tamCols = tamCols;
         this.tamRows = tamRows;
-        this.matriz = this.convertirFraccion(matriz);
-        this.matriz = this.convertirFraccion(matriz_2);
-        
         this.matriz = new Fracciones[tamRows][tamCols];
         this.matriz_2 = new Fracciones[tamRows_2][tamCols_2];
+        this.matriz = this.convertirFraccion(matriz);
+        this.matriz = this.convertirFraccion(matriz_2);
+
     }
     
     public Fracciones[][] convertirFraccion(int[][] matriz)
@@ -54,6 +54,7 @@ public class Operaciones {
         return nuevaMatriz;
     }
     
+    // Este metodo invierte filas y columnas
     public Fracciones[][] sacarTrasversal()
     {
         if(tamCols == tamRows)
@@ -71,31 +72,41 @@ public class Operaciones {
         return null;
     }
     
-    public Fracciones calcularDeterminante()
+    public Fracciones calcularDeterminante(Fracciones[][] matriz)
     {
+        // Comprueba si el número de columnas y filas son iguales
         if(this.tamRows == this.tamCols)
         {
+            // Declaramos variables auxiliares
             Fracciones[] fr = new Fracciones[tamRows];
-            Fracciones aux = matriz[0][0];
-            int count; 
+            Fracciones aux = matriz[0][0]; 
             Fracciones sumar = new Fracciones(0);
             Fracciones restar = new Fracciones(0);
+            int count;
             
+            // Repetir 2 veces. Primera vez se suman, la otra restan
             for(int oneTwo=0; oneTwo<2; oneTwo++)
             {
                 for(int i=0; i<tamRows; i++)
                 {
+                    // Restamos 0 o 1 dependiendo que ne vuelta va el ciclo
                     count = i-oneTwo;
-                    if(count == -1) count=2;
+                    // Si es -1 se convierte a 2 ya que aqui el -1 no nos sirve
+                    if(count == -1) count=2; 
+                    // Ciclo para hacer las multiplicaciones
                     for(int h=0; h<tamCols; h++)
                     {
+                        // Primera vuelta
                         if(h==0)
                             aux = matriz[h][count];
+                        // Ultima vuelta
                         else if(h==tamCols-1)
                             fr[i] = Fracciones.multiplicacion(aux, matriz[h][count]);
+                        // Si no es primer ni ultima vuelta
                         else
                             aux = Fracciones.multiplicacion(aux, matriz[h][count]);
 
+                        // Convierte a 0 o suma o resta la variable count dependiendo el caso
                         if(count == tamRows-1) 
                             count = 0; 
                         else if(oneTwo==0)
@@ -104,17 +115,21 @@ public class Operaciones {
                             count--;
                     }
                 }
+                // Si es la primer vuelta sumara todo
                 if(oneTwo==0)
                     sumar = Fracciones.suma(fr);
+                // Si es la segunda vuelta restara todo
                 else
                     restar = Fracciones.resta(fr);
             }
+            // Retorna el resultado final (La determinante)
             return Fracciones.resta(sumar, restar);
         }
         JOptionPane.showMessageDialog(null, "Sólo se puede sacar el determinante de una matriz de 3x3 por el momento");
         return null;
     }
     
+    // Este metodo comprueba si hay algun 0 o 1 en un arreglo
     public boolean allChecked(int[] check, int type)
     {        
         for(int i=0; i<check.length; i++)
@@ -128,6 +143,7 @@ public class Operaciones {
         return true;
     }
     
+    // Metodo que resete el arreglo de enteros recibido (Todo a 0)
     public void resetChecked(int[] check)
     {
         for(int i=0; i<check.length; i++)
@@ -215,12 +231,71 @@ public class Operaciones {
         return matrizGauss;
     }
     
-    public void metodoDeterminantes()
+    public Fracciones[][] sustituirColumna(int column)
     {
-        
+        Fracciones[][] nuevaMatriz = new Fracciones[tamCols][tamRows];
+        for(int cols=0; cols<tamCols; cols++)
+        {
+            for(int rows=0; rows<tamRows; rows++)
+            {
+                if(cols == column)
+                    nuevaMatriz[cols][rows] = matriz_2[cols][rows];
+                else
+                    nuevaMatriz[cols][rows] = matriz[cols][rows];
+            }
+        }
+        return nuevaMatriz;
     }
     
-    public void inversaGaussJordan()
+    public String metodoDeterminantes()
+    {
+        // Caculamos la determinante total y la guardamos en una variable
+        Fracciones dt = this.calcularDeterminante(this.matriz);
+        // Declaramos varibles de determinante en X, determinante en Y y determiante en Z
+        Fracciones dx, dy, dz, x, y, z;
+        // Declaramos el arreglo bidimensional donde se sustituiran columans
+        Fracciones[][] mtzSustituida;
+        
+        // Ahora sustituimos la columna de resultados en la primer columna y calculamos el determinante
+        mtzSustituida = this.sustituirColumna(0);
+        dx = this.calcularDeterminante(mtzSustituida);
+        // Sacamos X dividiendo Dx entre Dt
+        x = Fracciones.division(dx, dt);
+        
+        // Ahora sustituimos la columna de resultados en la segunda columna y calculamos el determinante
+        mtzSustituida = this.sustituirColumna(1);
+        dy = this.calcularDeterminante(mtzSustituida);
+        // Sacamos Y dividiendo Dy entre Dt
+        y = Fracciones.division(dy, dt);
+        
+        // Por ultimo sustituimos columna de resultados en la tercer columna y calculamos el determinante
+        mtzSustituida = this.sustituirColumna(2);
+        dz = this.calcularDeterminante(mtzSustituida);
+        // Sacamos Z dividiendo Dz entre Dt
+        z = Fracciones.division(dz, dt);
+        
+        return "X="+x+"  Y="+y+"  Z="+z;
+    }
+    
+    public Fracciones[][] inversaGaussJordan()
+    {
+        Fracciones dt = this.calcularDeterminante(this.matriz);
+        if(dt.GetNumerador() != 0)
+        {
+            if(tamCols == tamRows)
+            {
+                this.setMatriz_2(this.matrizIdentidad());
+                return this.metodoGaussJordan();
+            } 
+            else
+                JOptionPane.showMessageDialog(null, "Error. Las filas y columnas de la matriz deben ser iguales.");
+        }
+        else
+            JOptionPane.showMessageDialog(null, "Error. El determinante es 0 por lo tanto no se puede sacar la inversa");
+        return null;
+    }
+    
+    public void comprobacionInvGJ()
     {
         
     }
@@ -228,5 +303,26 @@ public class Operaciones {
     public void inversaCofactores()
     {
         
+    }
+    
+    public Fracciones[][] matrizIdentidad()
+    {
+        Fracciones[][] ident = new Fracciones[tamCols][tamRows];
+        for(int rows=0; rows<ident.length; rows++)
+        {
+            for(int cols=0; cols<ident[rows].length; cols++)
+            {
+                if(rows == cols)
+                    ident[rows][cols] = new Fracciones(1);
+                else
+                    ident[rows][cols] = new Fracciones(0);
+            }
+        }
+        return ident;
+    }
+    
+    public void setMatriz_2(Fracciones[][] matriz)
+    {
+        this.matriz_2 = matriz;
     }
 }
